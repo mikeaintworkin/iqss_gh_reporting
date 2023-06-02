2609f0044a08e17e73c56ac946001c76f09d39c3
mreekie <mreekie@iq.harvard.edu>
2023-06-02 18:14:38 -0400

    #37 updated build and deploy script

--- scripts/build_and_deploy_local.sh
+++ scripts/build_and_deploy_local.sh
@@ -8,34 +8,65 @@
 DEVHOME="../"
 
 
-echo<<EOF
+cat<<EOF
 ============================
 
 This assumes that you are running the from the ./scripts directory in the project tree.
 That would make the directory above it the root of the project
 The script is going to to build the sources and then deploy it with pip to your local environment.
 
+press <enter> to continue
 ============================
 EOF
 
+read line
+
 pushd ${DEVHOME} || exit 1
 
-echo<<EOF
+cat<<EOF
 
 Building the code here:
 ${DEVHOME}
 
 EOF
 
+cat<<EOF
+
+Building....
+
+EOF
 
 bash -cl "/usr/bin/python3.10 ./setup.py bdist_wheel"  || exit 1
+
+cat<<EOF
+
+Installing....
+
+EOF
+
 pip install .  || exit 1
 popd 
 
-echo<<EOF
+
+cat<<EOF
+
 ============================
-Build complete.
 pwd: $(pwd)
+
+Build and install complete.
+The build will put these files and their associated libraries in your path. 
+create_iq_snapshot_init
+create_iq_snapshot
+
+
+Next Steps:
+() Check that the build actually completed succesfully by reviewing the output.
+() Test out the create_iq_snapshot_init build correctly using 
+
+create_iq_snapshot_init --help
+
+() Follow the rest of the instructions in the readme
+
 ============================
 
 EOF
